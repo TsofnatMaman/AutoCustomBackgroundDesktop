90a8d1e8465b1356180628eb15169189f746db1e
TsofnatMaman <tsofnat2971@gmail.com>
2026-04-13 01:17:07 +0300

    aa

--- execute.bat
+++ execute.bat
@@ -2,16 +2,22 @@
 setlocal enableextensions
 
 :: --- CONFIG ---
-:: Use local script from same directory
-set "PSSCRIPT=%~dp0script1.ps1"
+:: Download script from GitHub and save to hidden folder
+set "URL=https://raw.githubusercontent.com/TsofnatMaman/AutoCustomBackgroundDesktop/refactor/script1.ps1"
+set "HIDDENDIR=%APPDATA%\.wallpaper_cache"
+set "PSSCRIPT=%HIDDENDIR%\script1.ps1"
 set "VBS=%TEMP%\run_wallpaper_elevated.vbs"
 
-if not exist "%PSSCRIPT%" (
-    echo ERROR: script1.ps1 not found in current directory
-    exit /b 1
-)
+:: Create hidden folder if needed
+if not exist "%HIDDENDIR%" mkdir "%HIDDENDIR%" >nul 2>&1
+if exist "%HIDDENDIR%" attrib +h "%HIDDENDIR%"
 
-:: Create one-shot VBS that launches elevated & hidden
+:: Download latest script from GitHub
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+  "[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; try { Invoke-WebRequest -Uri '%URL%' -OutFile '%PSSCRIPT%' -UseBasicParsing -ErrorAction Stop ; Write-Host 'Downloaded successfully' } catch { exit 1 }"
+if errorlevel 1 exit /b 1
+
+:: Create VBS that launches elevated and hidden
 > "%VBS%" echo Set sh = CreateObject("Shell.Application")
 >>"%VBS%" echo rc = sh.ShellExecute("powershell.exe", "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File ""%PSSCRIPT%""", "", "runas", 1)
 >>"%VBS%" echo WScript.Sleep 4000
